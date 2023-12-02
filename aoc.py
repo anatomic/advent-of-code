62322314f6efd0309fc7d4cc572a48f6aa893a75
Ian Thomas <iant@meta.com>
2023-12-02 14:11:40 +0000

    Add a little cli to fetch input & add new script

---
+++ aoc.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+import subprocess
+import sys
+
+session = os.environ["AOC_SESSION"]
+
+cli = argparse.ArgumentParser(description="Fetch the input for a specific day")
+cli.add_argument("--year", type=int, default=2023, help="Year to fetch")
+cli.add_argument("--day", type=int, default=1, help="Day to fetch")
+cli.add_argument(
+    "--init", help="Create the empty script for the day", action="store_true"
+)
+
+args = cli.parse_args()
+
+useragent = "https://github.com/anatomic/advent-of-code by ian@ian-thomas.net"
+cmd = f'curl https://adventofcode.com/{args.year}/day/{args.day}/input --cookie "session={session}" -A {useragent}'
+output = subprocess.check_output(cmd, shell=True)
+output = output.decode("utf-8")
+
+open(f"{args.year}/input/day{args.day}.txt", "w").write(output.strip())
+
+if args.init:
+    open(f"{args.year}/day{args.day}.py", "w").write(
+        """import os
+import re
+current_file = os.path.basename(__file__)
+current_path = os.path.relpath(__file__)
+year = current_path.split("/")[0]
+day, ext = os.path.splitext(current_file)
+
+f = open(f"{year}/input/{day}.txt", "r")
+input = f.read()
+"""
+    )
+
+print("\n".join(output.split("\n")[:10]), file=sys.stderr)
