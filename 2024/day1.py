d1d76c8bf4a18e63cd25ea004006c7b3e5a38340
Ian Thomas <iant@meta.com>
2024-12-02 07:02:42 +0000

    2024 day 1

--- 2024/day1.py
+++ 2024/day1.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from pathlib import Path
 
 current_path = Path(__file__).resolve()
@@ -5,4 +6,23 @@ year = current_path.parts[-2]
 day = current_path.stem
 
 f = open(f"{year}/input/{day}.txt", "r")
-input = f.read()
+input = f.readlines()
+
+a = list()
+b = list()
+
+for line in input:
+    [l, r] = line.split()
+    l, r = int(l), int(r)
+    a.append(l)
+    b.append(r)
+
+a.sort()
+b.sort()
+part1 = sum([abs(b - a) for a, b in zip(a, b)])
+
+print(f"part1: {part1}")
+
+c = Counter(b)
+part2 = sum([a * c[a] for a in a])
+print(f"part2: {part2}")
