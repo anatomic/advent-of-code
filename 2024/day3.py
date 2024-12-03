d2ce2c4b94eeb2f16ddaf67c53ffa31f67c03ba2
Ian Thomas <iant@meta.com>
2024-12-03 10:34:55 +0000

    2024 day 3

--- 2024/day3.py
+++ 2024/day3.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 current_path = Path(__file__).resolve()
@@ -6,3 +7,23 @@ day = current_path.stem
 
 f = open(f"{year}/input/{day}.txt", "r")
 input = f.read()
+ins = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+
+
+def parse_and_sum(text):
+    return sum([int(l) * int(r) for l, r in ins.findall(text)])
+
+
+valid = ""
+v = True
+
+for i in range(len(input)):
+    if input[i:].startswith("do()"):
+        v = True
+    if input[i:].startswith("don't()"):
+        v = False
+    if v:
+        valid += input[i]
+
+print("part1", parse_and_sum(input))
+print("part2", parse_and_sum(valid))
